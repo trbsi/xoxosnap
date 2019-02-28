9394ac20147eb36e2bb6252766eccfa14c775c43
Dario Trbovic <timmy.dario@gmail.com>
2019-02-28 20:19:44 +0100

    Add intervention/image

--- config/app.php
+++ config/app.php
@@ -177,8 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-
+        //Custom
         App\Providers\ViewServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -228,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
